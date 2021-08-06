5c7e49fb62b7b6df5a1d281ea238ab22ca0c1283
Bary Pollack <bary.pollack@baker.edu>
2021-08-06 01:00:00 -0700

    Added no friction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
+
+    // FixedUpdate is called once every physics update (default 100x/second)
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 01)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            rigidbodyComponent.AddForce(5 * Vector3.up, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+
 }
